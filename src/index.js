b14c4557bef4b337bc2271109a5413c19f59622f
Peter Hiza <peter_hiza@external.mckinsey.com>
2023-02-03 15:49:23 +0100

    clean up repo

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import { createClient as createWSClient } from 'graphql-ws';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const BACKEND_PORT = 3001;
 // switch to this to connect directly to the service
@@ -37,8 +35,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
